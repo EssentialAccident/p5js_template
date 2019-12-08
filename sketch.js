41d9b8d2d29a0aa9d4c0e47396ecce04099419e2
Lino Oropeza <linooropeza@gmail.com>
2019-12-08 15:47:37 -0500

    Includes the files to use this as a template to work with the p5js library."

---
+++ sketch.js
@@ -0,0 +1,12 @@
+// Global variables
+
+
+function setup() {
+    // Setup for the sketch
+    createCanvas(600, 600);
+}
+
+function draw() {
+    // Draws the sketch 60 times a second
+    background(51);
+}
